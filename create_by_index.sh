75c72185f6b805459b3538a7c5ae6d46732375c6
Ilya Dobryakov <ilya.dobryakov@icloud.com>
2023-12-31 01:21:09 +0300

    some mods update + possible continuous textures fix

--- create_by_index.sh
+++ create_by_index.sh
@@ -17,6 +17,6 @@ fi
 while IFS=','; read -r mcV qV mN mV; do
   echo "Сreating modpack for args: \"$mcV\" \"$qV\" \"$mN\" \"$mV\""
 
-  ./create_base.sh "$mcV" "$qV" "$mN" "$mV" || \
+  ./create_base.sh "$mcV" "$qV" "$(echo "$mN" | sed -r 's/_/\ /g')" "$mV" || \
     echo "Can't create modpack \"$(echo "$mN" | tr -d '\\') $mcV-$mV\"";
 done <<< "$(./get_version_by_index.sh "$1")"
